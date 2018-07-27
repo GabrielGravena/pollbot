a7d162f3d00b618f1c09bce2e7af3560580c30dd
Gabriel Gravena Neves <gabriel_gravena@hotmail.com>
2018-07-26 22:42:34 -0700

    Create polloptions table

--- db.js
+++ db.js
@@ -38,6 +38,7 @@ module.exports.Create = async function()
 
     await db.runAsync("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
     await db.runAsync("CREATE TABLE IF NOT EXISTS polls (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
+    await db.runAsync("CREATE TABLE IF NOT EXISTS polloptions (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, pollid INTEGER)");
 
     return db;
 }
